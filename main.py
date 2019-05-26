bcedbfb509dfd3f5553d85ea1b8cf949038f4c21
Vivek Verma <vivnps.verma@gmail.com>
2019-05-25 21:03:18 -0700

    Follower Bot

--- main.py
+++ main.py
@@ -3,13 +3,18 @@ import sys
 import os
 
 sys.path.append(os.path.join(sys.path[0], '../'))
-args = {i.split(":")[0]:i.split(":")[1] for i in open("config").read().strip().split("\n")}
-print(args)
+args = {i.split(":")[0]: i.split(":")[1]
+        for i in open("pw.config", "r").read().strip().split("\n")}
 
-bot = Bot(filter_private_users=False, filter_users_without_profile_photo=False,
-          max_follows_per_day=1000, min_media_count_to_follow=0, follow_delay=3) # Play around with these settings
-bot.login(username=args.u, password=args.p,
-          proxy=args.proxy)
 
-for username in args.users:
+bot = Bot(
+    filter_private_users=False,
+    filter_users_without_profile_photo=False,
+    max_follows_per_day=1000,
+    min_media_count_to_follow=0,
+    follow_delay=3
+)  # Play around with these settings
+bot.login(username=args['username'], password=args['password'])
+
+for username in eval(args['users']):
     bot.follow_followers(username)
